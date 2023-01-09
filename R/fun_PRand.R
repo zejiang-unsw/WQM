3301624faf39e697e6316c3641372a155740e5d6
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2023-01-09 19:38:26 +1100

    Update fun_PRand.R
    set.seed for shuffling process

--- R/fun_PRand.R
+++ R/fun_PRand.R
@@ -2,34 +2,46 @@
 #'
 #' @param modulus  Modulus of complex values.
 #' @param phases  Argument of complex values.
-#' @param noise_mat Complex matrix from a random time series
+#' @param noise_mat Complex matrix from random time series.
 #' @param method Shuffling method, M1: non-shuffling and M2: shuffling. M2 by default.
+#' @param seed Seed for shuffling process.
 #'
 #' @return A new complex matrix
 #' @export
 #'
-prsim <- function(modulus, phases, noise_mat, method=c("M1","M2")[2]){
+prsim <- function(modulus, phases, noise_mat, method=c("M1","M2")[2], seed=100){
+
+    if(!is.null(seed)) set.seed(seed)
 
     ###===============================###===============================###
     ### use the noise matrix corresponding to this run
-    tmp <- Arg(noise_mat)
+    mat_new <- vector("list",length(noise_mat))
+    for(r in 1:length(noise_mat)){
 
-    if(method=="M1"){ # no reshuffle
-      phases <- tmp
-    } else if(method=="M2"){
-      ord.bcf <- apply(phases, 2, order)
-      tmp.rank <- apply(tmp, 2, sort)
-      tmp.n <- sapply(1:ncol(tmp), function(ii) {tmp[ord.bcf[,ii],ii] <- tmp.rank[,ii];
-      return(tmp[,ii])})
+      tmp <- Arg(noise_mat[[r]])
 
-      shuff <- ShuffleBlocks(1:nrow(phases), size=12)
-      shuff <- shuff[!shuff>nrow(phases)]
+      if(method=="M1"){ # no reshuffle
+        phases <- tmp
+      } else if(method=="M2"){
 
-      phases <- tmp.n[shuff,]
-      modulus <- modulus[shuff,]
+        ord.bcf <- apply(phases, 2, order)
+        tmp.rank <- apply(tmp, 2, sort)
+        tmp.n <- sapply(1:ncol(tmp), function(ii) {tmp[ord.bcf[,ii],ii] <- tmp.rank[,ii];
+        return(tmp[,ii])})
+
+        shuff <- ShuffleBlocks(1:nrow(phases), size=6)
+        shuff <- shuff[!shuff>nrow(phases)]
+
+        phases <- tmp.n[shuff,]
+        modulus <- modulus[shuff,]
+        #if(r==1|r==2) cat('r',r,":",shuff,'-------')
+      }
+
+      mat_new[[r]] <- matrix(complex(modulus=modulus,argument=phases),ncol=ncol(phases))
+
     }
 
-    mat_new <- matrix(complex(modulus=modulus,argument=phases),ncol=ncol(phases))
+    if(!is.null(seed)) rm(.Random.seed, envir=.GlobalEnv)
 
     return(mat_new)
 }
@@ -55,8 +67,8 @@ ShuffleBlocks <- function(v, size = 6L) {
   v <- 1: ((length(v) %/% size+1)*size)
 
   mat <- matrix(v, nrow = size)
-  #out <- as.vector(apply(mat, 2, sample)) # random sample within block
-  out <- as.vector(apply(mat, 2, function(x) guyrot(x,sample(-size:size,1)))) #rotate within block
+  out <- as.vector(apply(mat, 2, sample)) # random sample within block
+  #out <- as.vector(apply(mat, 2, function(x) guyrot(x,sample(1:size,1)))) #rotate within block
   #out <- as.vector(mat[, sample(ncol(mat))]) # block shuffle
   #print(out)
   return(out)
