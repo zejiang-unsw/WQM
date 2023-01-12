e27e3cfe0475b3c5468ea61aafa8d8b5b48eba07
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2023-01-13 09:37:40 +1100

    update data

---
+++ R/sampledata.R
@@ -0,0 +1,16 @@
+#'@title Sample data: Rainfall forecasts data
+#'@description A dataset containing 2 stations including observation and raw forecasts.
+#'
+#'@docType data
+#'@keywords datasets
+#'@name sample
+#'@usage data(sample)
+NULL
+
+#'@title Australia NWP rainfall forecasts at lead 1h over Sydney region
+#'@description A dataset containing 160 stations including observation and raw forecasts.
+#'@docType data
+#'@keywords datasets
+#'@name NWP.rain
+#'@usage data(NWP.rain.h)
+NULL
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,6 +2,7 @@
 ## ACCESS NWP model----
 library(dplyr)
 library(data.table)
+
 load(paste0("MWR2022/data-raw/dat.tmp.Rdata"))
 
 #summary(dat.tmp)
@@ -11,17 +12,18 @@ Ind.samp <- c(34,75)
 station.no[Ind.samp]
 
 #------------------------------------------------------------------------------#
-###sample station
+###sample stations
 dat.tmp.n <- dat.tmp %>% subset(Lead==1)
 NWP.rain.samp <- dat.tmp.n[, c(1:3, Ind.samp+3)] %>% data.table() %>% melt(id=1:3, variable="Station", value="Rain") %>%
   dcast(Lead+Date+Station~Model, value.var = 'Rain') %>% mutate_if(is.factor,as.character) #%>%
 
-data.h.df <- NWP.rain.samp %>% na.omit() %>% arrange(Date) %>%
-  group_by(Station) %>%  group_split() %>% lapply(FUN=as.data.frame)
+sample <- NWP.rain.samp %>% na.omit() %>% arrange(Date) %>%
+  group_by(Station) %>% group_split() %>% lapply(FUN=as.data.frame)
 
-usethis::use_data(data.h.df, overwrite = TRUE)
+usethis::use_data(sample, overwrite = TRUE)
+
 ###all stations
-NWP.rain.h <- dat.tmp %>% subset(Lead==1) %>% data.table() %>% melt(id=1:3, variable = "Station", value = "Rain") %>%
-  dcast(Lead+Date+Station~Model, value.var = 'Rain') %>% mutate_if(is.factor,as.character)
+NWP.rain <- dat.tmp %>% subset(Lead==1) %>% data.table() %>% melt(id=1:3, variable = "Station", value = "Rain") %>%
+  dcast(Lead+Date+Station~Model, value.var = 'Rain') %>% mutate_if(is.character,as.factor)
 
-usethis::use_data(NWP.rain.h, overwrite = TRUE)
+usethis::use_data(NWP.rain, overwrite = TRUE)
